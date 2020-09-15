dfd2eb01511e4ae65f239c0daa55d3beae76014e
Stephen Reynolds <stephenreynolds@Stephens-iMac.local>
2020-09-15 18:21:04 +0100

    Module 1 Assignment Upload

---
+++ module1-solution/app.js
@@ -0,0 +1,39 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+
+
+
+
+
+    $scope.foodCount = function () {
+
+      if (($scope.items === undefined) || (!$scope.items.replace(/\s/g, ''))){
+
+          $scope.message = "Please enter data first"
+
+      }else if (($scope.items.split(",").length < 4) && ($scope.items.split(",").length > 0)){
+
+           $scope.message = "Enjoy";
+
+      }else if ($scope.items.length >= 4) {
+
+            $scope.message = "Too Much"
+
+      }
+
+
+    };
+
+}
+
+
+})();
